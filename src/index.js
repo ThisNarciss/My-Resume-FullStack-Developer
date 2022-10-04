82c09f510e8210faccec7b50f040c4e92b5854e7
ThisNarciss <tyomaxxx000@gmail.com>
2022-10-04 18:54:09 +0300

    adds slider in site

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './js/anchor-scroll';
+import './js/simple-light-box';
+import './js/mobile-menu';
+import './js/slick-slider';
--- src/js/simple-light-box.js
+++ src/js/simple-light-box.js
@@ -1,6 +1,9 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-var lightbox = $('.gallery a').simpleLightbox({
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  animationSpeed: 1000,
+
   captionDelay: 250,
   captionsData: 'alt',
 });
